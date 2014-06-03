b645c28bad2d5a3f18ecb0ca22b906d657b85e46
dane-lyon <dane-lyon@users.noreply.github.com>
2014-06-03 10:56:03 +0200

    Update modifproxyesu.sh

--- modifproxyesu.sh
+++ modifproxyesu.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # remplacement valeur du proxy dans  la configuration ESU
 cp -a /home/esu /home/esu.sav
-read -p "Donnez l'ip de l'ancien proxu dans esu ?" ancienproxy
-read -p "Donnez la valeur du nouveau proxy ?" nouveauproxy
+read -p "Donnez l'ip de l'ancien proxu dans esu ? " ancienproxy
+read -p "Donnez la valeur du nouveau proxy ? " nouveauproxy
 # remplacement de l'ancienne valeur par la nouvelle valeur dans la configuration ESU existante
 sed -i  "s/$ancienproxy/$nouveauproxy/g" /home/esu/Base/*/*xml
 exit 0

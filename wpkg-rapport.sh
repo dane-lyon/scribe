5f06039350fdff2468b8791b49a707f03327e3b4
dane-lyon <dane-lyon@users.noreply.github.com>
2014-09-16 16:57:50 +0200

    Create wpkg-rapport.sh

---
+++ wpkg-rapport.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#installation de wpkg-report
+#
+#karim
+#
+#v2
+
+. ParseDico
+
+WPKGDIR="/home/wpkg"
+WPKGRAPPORT="$WPKGDIR/rapports"
+
+#on teste la présence de eole-wpkg
+dpkg -s eole-wpkg &>/dev/null
+if [ $? != 0 ] ; then
+    echo "Wpkg n'est pas installé sur ce serveur !"
+    exit 1
+else
+    cd $WPKGDIR/softwares
+    mkdir .EtatsClients &>/dev/null
+    setfacl -Rm u::-wx,g::-wx,o::-wx .EtatsClients
+    setfacl -Rdm u::rwx,g::rwx,o::rwx .EtatsClients
+    mkdir -p $WPKGRAPPORT &>/dev/null
+    cd $WPKGRAPPORT &>/dev/null
+    wget ftp://ftp.gig-mbh.de/software/wpkg-create-report.zip &>/dev/null
+    if [ $? != 0 ] ;then
+        echo "l'archive n'a pas pu être téléchargée !"
+        exit 1
+    else
+        echo "Téléchargement de l'archive OK !"
+        unzip -u wpkg-create-report.zip
+        rm -rf wpkg-create-report.zip
+        sed -i 's/<wpkgBaseDir><\/wpkgBaseDir>/<wpkgBaseDir>\\\\'$nom_machine'\\wpkg\\<\/wpkgBaseDir>/g' createReport.xml
+        sed -i 's/<clientStateStorePath>clientStates/<clientStateStorePath>\\\\'$nom_machine'\\wpkg\\softwares\\.EtatsClients\\/g' createReport.xml
+        sed -i 's/<saveOutput>false/<saveOutput>true/g' createReport.xml
+        sed -i 's/<outputFile>c:\\/<outputFile>\\\\'$nom_machine'\\wpkg\\rapports\\Compte-Rendu-/g' createReport.xml
+        echo "cscript createReport.js" > $WPKGRAPPORT/'Création du rapport WPKG.bat'
+        todos -u $WPKGRAPPORT/'Création du rapport WPKG.bat'
+        if [ $? = 0 ] ; then
+            echo "Creation du script de creation du rapport WPKG OK !"
+        fi
+        exit 0
+    fi
+fi

382411fa0809b28e805a0d54c34ab1ccaea50e1a
dane-lyon <dane-lyon@users.noreply.github.com>
2014-12-12 15:44:59 +0100

    Update InstallOpenERP_V2_basedeporte.sh

--- InstallOpenERP_V2_basedeporte.sh
+++ InstallOpenERP_V2_basedeporte.sh
@@ -101,8 +101,8 @@ Les anciens fichiers de configuration ont une extension en .BAK. Vous pouvez tou
 ##### Nouvelle fonction ####
 
 # Ajout d'un cron pour redémarrer chaque jour les services pour OpenERP 
-echo 30 23 * * * root $(dpkg -s openerp &>/dev/null) ; [[ $(echo $?) -eq 0 ]] && /etc/init.d/postgresql restart >> /etc/cron.d/purgeHOME
-echo 35 23 * * * root $(dpkg -s openerp &>/dev/null) ; [[ $(echo $?) -eq 0 ]] && /etc/init.d/openerp restart >> /etc/cron.d/purgeHOME
+echo 30 23 * * * root $(dpkg -s openerp &>/dev/null) ; [[ $(echo $?) -eq 0 ]] && /etc/init.d/postgresql restart > /etc/cron.d/openerp_restart
+echo 35 23 * * * root $(dpkg -s openerp &>/dev/null) ; [[ $(echo $?) -eq 0 ]] && /etc/init.d/openerp restart >> /etc/cron.d/openerp_restart
 
 # Déporte les bases d'OpenERP dans le home
 mkdir /home/openerp_bdd

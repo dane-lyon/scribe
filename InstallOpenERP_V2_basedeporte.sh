98751019e781cc410433f1f7972f37a799acf7c5
dane-lyon <dane-lyon@users.noreply.github.com>
2015-01-16 14:26:34 +0100

    Update InstallOpenERP_V2_basedeporte.sh

--- InstallOpenERP_V2_basedeporte.sh
+++ InstallOpenERP_V2_basedeporte.sh
@@ -101,21 +101,6 @@ Les anciens fichiers de configuration ont une extension en .BAK. Vous pouvez tou
 
 ##### Nouvelle fonction ####
 
-# Ajout d'un cron pour redémarrer chaque jour les services pour OpenERP 
-echo 30 23 * * * root $(dpkg -s openerp &>/dev/null) ; [[ $(echo $?) -eq 0 ]] && /etc/init.d/postgresql restart > /etc/cron.d/openerp_restart
-echo 35 23 * * * root $(dpkg -s openerp &>/dev/null) ; [[ $(echo $?) -eq 0 ]] && /etc/init.d/openerp restart >> /etc/cron.d/openerp_restart
-
-# Déporte les bases d'OpenERP dans le home
-mkdir /home/openerp_bdd
-mv /var/lib/postgresql/8.4/main/base /home/openerp_bdd
-ln -s /home/openerp_bdd/base /var/lib/postgresql/8.4/main/base
-chown -R postgres:postgres /home/openerp_bdd/base
-chmod -R u=rwx /home/openerp_bdd/base
-
-#! /bin/bash  
-
-if [ -f /etc/init.d/openerp ]; then
-
 # Ajout d'un cron pour redémarrer chaque jour les services pour OpenERP 
 echo "30 23 * * * root /etc/init.d/postgresql-8.4 restart" > /etc/cron.d/openerp_restart
 echo "35 23 * * * root /etc/init.d/openerp restart" >> /etc/cron.d/openerp_restart
@@ -133,10 +118,6 @@ chmod -R u=rwx /home/openerp_bdd/base
 
 /etc/init.d/postgresql-8.4 start
 /etc/init.d/openerp start
-
-else
-  exit
-fi
 
 exit 0
 fi

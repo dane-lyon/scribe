bbec891278db50fc1c57a0ce3e6e9235cf4ea1bd
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-09-15 14:12:29 +0200

    Update wpkg-rapport.sh

--- wpkg-rapport.sh
+++ wpkg-rapport.sh
@@ -37,8 +37,27 @@ else
         sed -i 's/<clientStateStorePath>clientStates/<clientStateStorePath>\\\\'$nom_machine'\\wpkg\\softwares\\.EtatsClients\\/g' createReport.xml
         sed -i 's/<saveOutput>false/<saveOutput>true/g' createReport.xml
         sed -i 's/<outputFile>c:\\/<outputFile>\\\\'$nom_machine'\\wpkg\\rapports\\Compte-Rendu-/g' createReport.xml
-        echo "cscript createReport.js" > $WPKGRAPPORT/'Création du rapport WPKG.bat'
-        todos -u $WPKGRAPPORT/'Création du rapport WPKG.bat'
+     
+
+
+      sed -i 's/<settings>/g' settings.xml 
+         sed -i 's/<generalSettings>/g' settings.xml 
+      sed -i 's/<wpkgBaseDir>\\192.168.220.10\wpkg\</wpkgBaseDir>/g' settings.xml 
+      sed -i 's/<clientStateStorePath>\\192.168.220.10\wpkg\softwares\.EtatsClients\</clientStateStorePath>/g' settings.xml 
+      sed -i 's/<ignoreExecuteAlways>false</ignoreExecuteAlways>/g' settings.xml 
+      sed -i 's/<packageStateFilter></packageStateFilter>/g' settings.xml 
+      sed -i 's/<hostFilter></hostFilter>/g' settings.xml 
+      sed -i 's/<columnFilter></columnFilter>/g' settings.xml 
+      sed -i 's/<outputProvider>html</outputProvider>/g' settings.xml 
+      sed -i 's/<saveOutput>true</saveOutput>/g' settings.xml 
+      sed -i 's/<outputFile>\\192.168.220.10\wpkg\rapports\Compte-Rendu-[YYYY]-[MM]-[DD]-[hh]-[mm].[ext]</outputFile>/g' settings.xml 
+      sed -i 's/<showOutput>true</showOutput>/g' settings.xml 
+   sed -i 's/</generalSettings>/g' settings.xml 
+        
+        
+        #echo "cscript createReport.js" > $WPKGRAPPORT/'Création du rapport WPKG.bat'
+        #todos -u $WPKGRAPPORT/'Création du rapport WPKG.bat'
+        mv run.wsf creation_du_rapport.wsf
         if [ $? = 0 ] ; then
             echo "Creation du script de creation du rapport WPKG OK !"
         fi

bb46d6a2c9904226b766579d7382e10685d7fa35
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-03-10 09:48:08 +0100

    Added files via upload
    Ce patch permet de modifier le fichier balado-connexion pour l'accès a balado en http

---
+++ patch-balado.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+clear
+# script qui copie & modifie le fichier /usr/share/eole/creole/distrib dans /usr/share/eole/creole/modif
+
+# copie du fichier a modifier dans le répertoire modif
+cp /usr/share/eole/creole/distrib/balado-connexion.php /usr/share/eole/creole/modif/balado-connexion.php
+
+#suppression du s de https
+sed -i "s/https/http/g" /usr/share/eole/creole/modif/balado-connexion.php
+
+# génération du patch
+gen_patch
+
+echo "pensez a lancer un reconfigure ensuite"
+

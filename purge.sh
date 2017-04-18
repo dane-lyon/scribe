d6c7d1c8b937ad5136ea2c4bbcc50b14cd252bc5
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2017-04-18 09:32:51 +0200

    Ajout log Nettoyage MBlock

--- purge.sh
+++ purge.sh
@@ -103,7 +103,7 @@ find /home -maxdepth 10 -type d -iregex '/home/.?/[^/]*/perso/\(.Config\|config_
 
 ####################
 ## Suppression des dossiers com.makeblock.Scratch.old_version
-
+ echo + Nettoyage MBlock
 version=$(grep \"version\" /home/wpkg/packages/mBLock.xml | cut -c33-37)
 find /home -maxdepth 6 -type d -iregex '/home/.?/[^/]*/perso/\(.Config\|config_eole\)/Application\ Data/com.makeblock.*' > dossier_mblock.txt
 while read ligne

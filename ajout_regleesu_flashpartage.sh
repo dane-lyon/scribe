d32ef3eaab973e308b9e6a209a231570c97bad3c
Simon B <sibe39@users.noreply.github.com>
2017-12-19 11:37:25 +0100

    Create ajout_regleesu_flashpartage.sh

---
+++ ajout_regleesu_flashpartage.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+objectif :
+- Dans le fichier "/home/esu/Console/ListeRegles.xml", insérer les lignes suivantes :
+
+						<Regle type="BOOLEEN">
+							<OS>254</OS>
+							<Intitule>Autoriser la lecture des animations flash (swf) sur un partage réseau</Intitule>
+							<Chemin>JS://%AppData%\Mozilla\Firefox\Profiles\Defaut\user.js</Chemin>
+							<Variable nom="plugins.flashBlock.enabled" type="BOOLEAN">
+								<ValueOn>false</ValueOn>
+								<ValueOff>true</ValueOff>
+							</Variable>
+							<Commentaire>Mettre sur false pour autoriser la lecteure flash depuis partage réseau</Commentaire>
+						</Regle>
+
+- Juste après les lignes :
+
+						<Regle type="BOOLEEN">
+							<OS>254</OS>
+							<Intitule>Vérifier que Firefox est le navigateur par défaut</Intitule>
+							<Chemin>JS://%AppData%\Mozilla\Firefox\Profiles\Defaut\user.js</Chemin>
+							<Variable nom="browser.shell.checkDefaultBrowser" type="BOOLEAN">
+								<ValueOn>true</ValueOn>
+								<ValueOff>false</ValueOff>
+							</Variable>
+							<Commentaire>Pas de commentaires pour cette règle !</Commentaire>
+						</Regle>
+            
+
+================================
+
+
+sed -n '/<Variable nom="browser.shell.checkDefaultBrowser" type="BOOLEAN">
+								<ValueOn>true</ValueOn>
+								<ValueOff>false</ValueOff>
+							</Variable>
+							<Commentaire>Pas de commentaires pour cette règle !</Commentaire>/ 
+<Variable nom="browser.shell.checkDefaultBrowser" type="BOOLEAN">
+								<ValueOn>true</ValueOn>
+								<ValueOff>false</ValueOff>
+							</Variable>
+							<Commentaire>Pas de commentaires pour cette règle !</Commentaire>
+						<Regle type="BOOLEEN">
+							<OS>254</OS>
+							<Intitule>Autoriser la lecture des animations flash (swf) sur un partage réseau</Intitule>
+							<Chemin>JS://%AppData%\Mozilla\Firefox\Profiles\Defaut\user.js</Chemin>
+							<Variable nom="plugins.flashBlock.enabled" type="BOOLEAN">
+								<ValueOn>false</ValueOn>
+								<ValueOff>true</ValueOff>
+							</Variable>
+							<Commentaire>Mettre sur false pour autoriser la lecteure flash depuis partage réseau</Commentaire>
+						</Regle>'
+
+/home/esu/Console/ListeRegles.xml
